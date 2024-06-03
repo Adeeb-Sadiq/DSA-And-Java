365370f4d907254286850215b45464e7c3c96dfb
Adeeb <adeeb5.sadiq7@gmail.com>
2024-06-03 18:09:48 +0530

    solved missing number problem

--- concepts/sorting/cycleSort/CyclicSort.java
+++ concepts/sorting/cycleSort/CyclicSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CyclicSort {
     public static void main(String[] args) {
-        int[] arr = {4,2,1,5,3};
+        int[] arr = {4,6,2,1,5,3};
         cyclicSortOwn(arr);
         System.out.println(Arrays.toString(arr));
 
