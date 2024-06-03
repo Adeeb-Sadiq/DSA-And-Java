56f5e6c0e591d3f89f0cbfd0e94eb29741ff9044
Adeeb <adeeb5.sadiq7@gmail.com>
2024-06-03 16:48:26 +0530

    added cyclic sort algorithm

---
+++ concepts/sorting/cycleSort/CyclicSort.java
@@ -0,0 +1,27 @@
+package concepts.sorting.cycleSort;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {4,2,1,5,3};
+        cyclicSortOwn(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    // tried on my own to make the algorithm, and it works, O(n)
+    public static void cyclicSortOwn(int[] arr) {
+        int i = 0;
+        while(i < arr.length) {
+            if(i == arr[i] - 1) {
+                i++;
+            } else {
+                int index = arr[i] - 1;
+                int temp = arr[index];
+                arr[index] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+    }
+}
