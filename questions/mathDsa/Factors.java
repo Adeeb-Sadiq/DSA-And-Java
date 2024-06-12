c814facc22e4cd015b914868c85fd15d00558357
Adeeb <adeeb5.sadiq7@gmail.com>
2024-06-13 00:35:35 +0530

    maths problems

---
+++ questions/mathDsa/Factors.java
@@ -0,0 +1,18 @@
+package questions.mathDsa;
+
+public class Factors {
+    public static void main(String[] args) {
+        factors(360);
+    }
+
+    private static void factors(int n) {
+        int count = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if(n % i == 0) {
+                System.out.print(i + " " + n / i + " ");
+                count++;
+            }
+        }
+        System.out.printf("%n%d", count * 2);
+    }
+}
