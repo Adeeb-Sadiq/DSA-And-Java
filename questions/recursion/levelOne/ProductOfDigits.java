2a636d865049d5bba261a847693e65baf85da0ec
Adeeb <adeeb5.sadiq7@gmail.com>
2024-06-13 18:35:00 +0530

    recursion easy questions

---
+++ questions/recursion/levelOne/ProductOfDigits.java
@@ -0,0 +1,14 @@
+package questions.recursion.levelOne;
+
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        System.out.println(prod(5283));
+    }
+
+    public static int prod(int n) {
+        if (n < 10) {
+            return n;
+        }
+        return n%10 * prod(n/10);
+    }
+}
---
+++ questions/recursion/levelOne/ReverseNumber.java
@@ -0,0 +1,25 @@
+package questions.recursion.levelOne;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int n = 56032;
+        int base = nthbase(n);
+        System.out.println(reverseInt(n, base));
+    }
+
+    public static int reverseInt(int n, int base) {
+        if (n < 10) {
+            return n;
+        }
+        return reverseInt(n / 10, base - 1) + (n % 10) * (int)(Math.pow(10, base));
+    }
+
+    public static int nthbase(int n) {
+        int power = 0;
+        while(n > 0) {
+            n = n/10;
+            power++;
+        }
+        return power - 1;
+    }
+}
