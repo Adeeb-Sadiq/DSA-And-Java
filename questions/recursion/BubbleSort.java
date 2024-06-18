a3ab810d8091c468da263b2ddd6d082ae3b832b1
Adeeb <adeeb5.sadiq7@gmail.com>
2024-06-19 01:59:10 +0530

    patterns and bubble sort using recursion

---
+++ questions/recursion/BubbleSort.java
@@ -0,0 +1,27 @@
+package questions.recursion;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,6,3,2,1};
+        bubble(arr, arr.length - 1, 0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubble(int[] arr, int r, int c) {
+        if(r == 0) {
+            return;
+        }
+        if(c < r) {
+            if(arr[c] > arr[c+1]) {
+                int t = arr[c];
+                arr[c] = arr[c+1];
+                arr[c+1] = t;
+            }
+            bubble(arr, r, c + 1);
+        } else {
+            bubble(arr, r - 1, 0);
+        }
+    }
+}
---
+++ questions/recursion/patterns/Patterns.java
@@ -0,0 +1,34 @@
+package questions.recursion.patterns;
+
+public class Patterns {
+    public static void main(String[] args) {
+        pattern2(4, 0);
+    }
+
+    public static void pattern3(int r, int c) {
+        if(r == 0) {
+            return;
+        }
+        if(c < r) {
+            System.out.print("* ");
+            pattern3(r, c + 1);
+        } else {
+            System.out.println();
+            pattern3(r - 1, 0);
+        }
+    }
+
+    public static void pattern2(int r, int c) {
+        if(r == 0) {
+            return;
+        }
+        if(c < r) {
+            pattern2(r, c + 1);
+            System.out.print("* ");
+        } else {
+            pattern2(r - 1, 0);
+            System.out.println();
+        }
+    }
+
+}
